f836bfcc8f635632b0063fd1d8bdf86a37de0d20
Jeffrey Luo <jeluo@redhat.com>
2023-06-20 11:16:35 -0400

    Bug: ACM-5052, Policy gets shortly into non-compliant state Solution: decode automatically converted stringData prior to evaluation
    Signed-off-by: Jeffrey Luo <jeluo@redhat.com>
    
    Bug: ACM-5052, Policy gets shortly into non-compliant state
    Solution: decode automatically converted stringData prior to evalutaion
    
    Signed-off-by: Jeffrey Luo <jeluo@redhat.com>
    
    Bug: ACM-5052, Policy gets shortly into non-compliant state
    Solution: decode automatically converted stringData prior to evalutaion
    
    Signed-off-by: Jeffrey Luo <jeluo@redhat.com>

---
+++ test/e2e/case32_secret_stringdata_test.go
@@ -0,0 +1,63 @@
+// Copyright Contributors to the Open Cluster Management project
+
+package e2e
+
+import (
+	"strconv"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"open-cluster-management.io/config-policy-controller/test/utils"
+)
+
+const (
+	case32ConfigPolicyName string = "case32config"
+	case32CreatePolicyYaml string = "../resources/case32_secret_stringdata/case32_create_secret.yaml"
+)
+
+var _ = Describe("Test converted stringData being decoded before comparison for Secrets", Ordered, func() {
+	It("Config should be created properly on the managed cluster", func() {
+		By("Creating " + case32ConfigPolicyName + " on managed")
+		utils.Kubectl("apply", "-f", case32CreatePolicyYaml, "-n", testNamespace)
+		cfg := utils.GetWithTimeout(clientManagedDynamic, gvrConfigPolicy,
+			case32ConfigPolicyName, testNamespace, true, defaultTimeoutSeconds)
+		Expect(cfg).NotTo(BeNil())
+	})
+
+	It("Verifies the config policy is initially compliant "+case32ConfigPolicyName+" in "+testNamespace, func() {
+		By("Waiting for " + case32ConfigPolicyName + " to become Compliant for up to " + strconv.Itoa(defaultTimeoutSeconds) + " seconds")
+		Eventually(func() interface{} {
+			cfgplc := utils.GetWithTimeout(
+				clientManagedDynamic, gvrConfigPolicy, case32ConfigPolicyName, testNamespace, true, defaultTimeoutSeconds,
+			)
+
+			return utils.GetComplianceState(cfgplc)
+		}, defaultTimeoutSeconds, 1).Should(Equal("Compliant"))
+	})
+
+	It("Verifies that a secret is created by "+case32ConfigPolicyName+" in openshift-config", func() {
+		By("Grabbing " + "htpasswd-secret from namespace openshift-config")
+		scrt := utils.GetWithTimeout(
+			clientManagedDynamic, gvrSecret, "htpasswd-secret", "openshift-config", true, defaultTimeoutSeconds,
+		)
+
+		Expect(scrt).NotTo(BeNil())
+	})
+
+	It("Verifies the config policy stays compliant "+case32ConfigPolicyName+" in "+testNamespace, func() {
+		By("Making sure " + case32ConfigPolicyName + " is consistently Compliant for " + strconv.Itoa(defaultTimeoutSeconds) + " seconds")
+		Consistently(func() interface{} {
+			cfgplc := utils.GetWithTimeout(
+				clientManagedDynamic, gvrConfigPolicy, case32ConfigPolicyName, testNamespace, true, defaultTimeoutSeconds,
+			)
+
+			return utils.GetComplianceState(cfgplc)
+		}, defaultTimeoutSeconds, 1).Should(Equal("Compliant"))
+	})
+
+	AfterAll(func() {
+		utils.Kubectl("delete", "configurationpolicy", case32ConfigPolicyName, "-n", testNamespace)
+		utils.Kubectl("delete", "secret", "htpasswd-secret", "-n", "openshift-config")
+	})
+})
